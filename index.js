d70fe6f4f526ffdaabb3dd6d83cfd0bb66d89867
vkrutsina <vera.krutsina@gmail.com>
2020-08-24 16:14:26 -0700

    game state variables

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ let player1Score = 0;
 let player2Score = 0;
 
 let player1Turn = true;
+
+const player1Dice = document.getElementById('player1Dice');
+const player2Dice = document.getElementById('player2Dice');
+let rollButton = document.getElementById('rollBtn');
+let resetButton = document.getElementById('resetBtn');
+let message = document.getElementById('message');
